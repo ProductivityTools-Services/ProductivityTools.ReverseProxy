790e1e2f61b9e276dda831c84b32025d563568f9
Pawel Wujczyk <pwujczyk@gmail.com>
2025-05-10 21:07:34 +0200

    bashwrite

--- CopySites-Availiable.sh
+++ CopySites-Availiable.sh
@@ -10,23 +10,23 @@ echo "$FILES"
 for fullpath in $FILES;
 do
  bashWrite "VERBOSE: --------------"
- echo "VERBOSE: processing file: $fullpath"
- echo "VERBOSE: copy $fullpath to sites-available"
+ bashWrite "VERBOSE: processing file: $fullpath"
+ bashWrite "VERBOSE: copy $fullpath to sites-available"
  cp $fullpath /etc/nginx/sites-available/
  FILENAME=$(basename $fullpath)
- echo "VERBOSE: only filename: $FILENAME"
- echo "VERBOSE: creating ln for $FILENAME"
+ bashWrite "VERBOSE: only filename: $FILENAME"
+ bashWrite "VERBOSE: creating ln for $FILENAME"
 
  SITEENABLEDPATH="/etc/nginx/sites-availiable/$FILENAME"
- echo "VERBOSE: site enabled path:$SITEENABLEDPATH"
+ bashWrite "VERBOSE: site enabled path:$SITEENABLEDPATH"
 
  SIMLINKPATH="/etc/nginx/sites-enabled/$FILENAME"
- echo "VERBOSE: simlink path: $SIMLINKPATH"
+ bashWrite "VERBOSE: simlink path: $SIMLINKPATH"
  
  if [[ -e $SIMLINKPATH ]]; then
-  echo "VERBOSE: simlink already  exists"
+  bashWrite "VERBOSE: simlink already  exists"
  else
-  echo "VERBOSE: File does not exist, creating simlink"
+  bashWrite "VERBOSE: File does not exist, creating simlink"
   ln -s "/etc/nginx/sites-available/$FILENAME" /etc/nginx/sites-enabled
 fi 
   sudo certbot --nginx -d $FILENAME --reinstall
@@ -37,7 +37,7 @@ done
 
 sudo nginx -t
 sudo nginx -s reload
-echo "VERBOSE: /etc/nginx/sites-enabled/ content:"
+bashWrite "VERBOSE: /etc/nginx/sites-enabled/ content:"
 ls -al /etc/nginx/sites-enabled/
 
 #cp -R ./sites-available/.  /etc/nginx/sites-available/
