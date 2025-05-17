9c263d30408faa6eba3e95b844ee39bb90de9179
Pawel Wujczyk <pwujczyk@gmail.com>
2025-05-17 22:56:02 +0200

    reformat

--- CopySites-Availiable.sh
+++ CopySites-Availiable.sh
@@ -6,13 +6,14 @@ if [ $# -eq 0 ]
     else
     VERBOSE_LEVEL=$1
 fi
+echo $VERBOSE_LEVEL
 
 
 function bashWrite {
   GLOBAL_VERBOSE_LEVEL=$1
   LOCAL_VERBOSE=$2
   TEXT=$3
-  if [[ $GLOBAL_VERBOSE_LEVEL -le $LOCAL_VERBOSE ]]
+  if [[ $GLOBAL_VERBOSE_LEVEL -ge $LOCAL_VERBOSE ]]
   then
   echo $TEXT
   fi
@@ -23,29 +24,33 @@ function writeSuccess {
  if [[ $SUCCESSTEXT == *"Congratulations! You have successfully enabled HTTPS on"* ]];then 
   echo "Congratulations! You have successfully enabled HTTPS on file above"
  fi
+ 
 }
 
-function processFile{
- bashWrite VERBOSE_LEVEL 1 "VERBOSE: --------------"
- bashWrite VERBOSE_LEVEL 1 "VERBOSE: processing file: $fullpath"
- bashWrite VERBOSE_LEVEL 2 "VERBOSE: copy $fullpath to sites-available"
+function processFile {
+ fullpath=$1
+ echo "FullPath:"
+ echo $fullpath
+ bashWrite $VERBOSE_LEVEL 1 "VERBOSE: --------------"
+ bashWrite $VERBOSE_LEVEL 1 "VERBOSE: processing file: $fullpath"
+ bashWrite $VERBOSE_LEVEL 2 "VERBOSE: copy $fullpath to sites-available"
  cp $fullpath /etc/nginx/sites-available/
  FILENAME=$(basename $fullpath)
- bashWrite VERBOSE_LEVEL 2 "VERBOSE: only filename: $FILENAME"
- bashWrite VERBOSE_LEVEL 2 "VERBOSE: creating ln for $FILENAME"
+ bashWrite $VERBOSE_LEVEL 2 "VERBOSE: only filename: $FILENAME"
+ bashWrite $VERBOSE_LEVEL 2 "VERBOSE: creating ln for $FILENAME"
 
  SITEENABLEDPATH="/etc/nginx/sites-availiable/$FILENAME"
- bashWrite VERBOSE_LEVEL 2 "VERBOSE: site enabled path:$SITEENABLEDPATH"
+ bashWrite $VERBOSE_LEVEL 2 "VERBOSE: site enabled path:$SITEENABLEDPATH"
  SIMLINKPATH="/etc/nginx/sites-enabled/$FILENAME"
- bashWrite VERBOSE_LEVEL 2 "VERBOSE: simlink path: $SIMLINKPATH"
+ bashWrite $VERBOSE_LEVEL 2 "VERBOSE: simlink path: $SIMLINKPATH"
  if [[ -e $SIMLINKPATH ]]; then
-  bashWrite VERBOSE_LEVEL 2 "VERBOSE: simlink already  exists"
+  bashWrite $VERBOSE_LEVEL 2 "VERBOSE: simlink already  exists"
  else
-  bashWrite VERBOSE_LEVEL 2 "VERBOSE: File does not exist, creating simlink"
+  bashWrite $VERBOSE_LEVEL 2 "VERBOSE: File does not exist, creating simlink"
   ln -s "/etc/nginx/sites-available/$FILENAME" /etc/nginx/sites-enabled
-fi 
+ fi 
   a=$(sudo certbot --nginx -d $FILENAME --reinstall 2>&1)
- # echo $a
+  echo $a
   writeSuccess "$a"
 }
 
@@ -53,9 +58,8 @@ FILES="./sites-available/*"
 echo "$FILES"
 
 for fullpath in $FILES;
- processFile $fullpath
 do
-
+ processFile $fullpath
 done
 
 
